085991a583fd2006f24c238d243d772094176667
MIGUEVARVAL <mvarvalenc26@gmail.com>
2025-06-02 01:09:09 -0500

    Se agrega la página con la información del estudiante y su historia académica

---
+++ src/app/components/students/students-info/students-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-students-info',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './students-info.component.html',
+  styleUrl: './students-info.component.scss'
+})
+export class StudentsInfoComponent {
+
+}
