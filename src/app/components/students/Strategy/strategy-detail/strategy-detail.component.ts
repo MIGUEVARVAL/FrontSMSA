66f455e2a657d16da36b19758e89122a6fa56775
MIGUEVARVAL <mvarvalenc26@gmail.com>
2025-06-09 11:55:57 -0500

    Se agregan constantes con la información de las estrategias y se modifica el html para que renderice toda la información

--- src/app/components/students/Strategy/strategy-detail/strategy-detail.component.ts
+++ src/app/components/students/Strategy/strategy-detail/strategy-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { StudentInfoService } from '../../../../services/student/student-info.service';
 
 @Component({
   selector: 'app-strategy-detail',
@@ -29,6 +30,42 @@ export class StrategyDetailComponent {
   protected errorMessage: string = "";
 
   /**
+   * Información del estudiante obtenida del servicio StudentInfoService
+   * @protected
+   * @type {any}
+   */
+  protected studentInfo: any = null;
+
+  protected strategy: any = {
+    id: 1,
+    titulo: 'Acompañamiento por tutor',
+    fecha: '22 de abril de 2025',
+    observaciones: 'Se asigna el tutor José Restrepo, profesor de planta con conocimientos en matemáticas. Esto con el fin de acompañar al estudiante en asignaturas como cálculo en varias variables la cual se le ha dificultado mucho al estudiantes Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, amet deserunt est perferendis sequi consectetur. Eligendi maxime similique eaque, fugit, aliquam natus quas fugiat quos doloremque provident quasi modi? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eaque, maiores ea dolore id a corporis voluptatibus cupiditate beatae rem distinctio quibusdam tempora rerum, eligendi assumenda! Explicabo laborum harum tenetur',
+    registros: [
+      {
+        id: 2,
+        fecha: '23 de abril de 2025',
+        user: {
+          nombre: 'Raul Restrepo',
+          cargo: 'Director Ingeniría Geológica'
+        },
+        titulo: 'Primera sesión de tutoría',
+        observaciones: 'El tutor José Restrepo ha comenzado a trabajar con el estudiante en las asignaturas de matemáticas y física.'
+      },
+      {
+        id: 1,
+        fecha: '22 de abril de 2025',
+        user: {
+          nombre: 'Melissa Velasco',
+          cargo: 'Unidad de Permanencia Facultad Minas'
+        },
+        titulo : 'Asignación de tutor',
+        observaciones: 'El tutor José Restrepo contacta al estudiante para coordinar las primeras sesiones de apoyo.'
+      }
+    ]
+  };
+
+  /**
    * Formulario reactivo para crear el seguimiento de una estrategia de apoyo al estudiante.
    * @protected
    * @type {FormGroup}
@@ -37,5 +74,17 @@ export class StrategyDetailComponent {
     title: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required])
   });
+
+  /**
+     * @param {StudentInfoService} studentInfoService servicio para obtener la información del estudiante
+     */
+  constructor(
+    private studentInfoService: StudentInfoService
+  ) { }
+
+  ngOnInit() {
+    // Se obtiene la información del estudiante al inicializar el componente
+    this.studentInfo = this.studentInfoService.getStudentInfo();
+  }
 
 }
--- src/app/components/students/Strategy/student-support-strategy/student-support-strategy.component.ts
+++ src/app/components/students/Strategy/student-support-strategy/student-support-strategy.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { StudentInfoService } from '../../../../services/student/student-info.service';
 
 @Component({
   selector: 'app-student-support-strategy',
@@ -28,6 +29,39 @@ export class StudentSupportStrategyComponent {
   protected errorMessage: string = "";
 
   /**
+   * Información del estudiante obtenida del servicio StudentInfoService
+   * @protected
+   * @type {any}
+   */
+  protected studentInfo: any = null;
+
+  /**
+   * Lista de estrategias de apoyo al estudiante.
+   * @protected
+   * @type {Array<any>}
+   */
+  protected strategies: any= [
+    {
+      id: 1,
+      title: 'Acompañamiento por tutor',
+      date: '22 de abril de 2025',
+      description: ' Se asigna el tutor José Restrepo, profesor de planta con conocimientos en matemáticas. Esto con el fin de acompañar al estudiante en asignaturas como cálculo en varias variables la cual se le ha dificultado mucho al estudiantes Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, amet deserunt est perferendis sequi consectetur. Eligendi maxime similique eaque, fugit, aliquam natus quas fugiat quos doloremque provident quasi modi? Doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eaque, maiores ea dolore id a corporis voluptatibus cupiditate beatae rem distinctio quibusdam tempora rerum, eligendi assumenda! Explicabo laborum harum tenetur'
+    },
+    {
+      id: 2,
+      title: 'Programa de Orientación Vocacional',
+      date: '22 de abril de 2025',
+      description: 'Ofrecer orientación vocacional para ayudar a los estudiantes a elegir su camino profesional.'
+    },
+    {
+      id: 3,
+      title: 'Asesoramiento Psicológico',
+      date: '22 de abril de 2025',
+      description: 'Brindar apoyo psicológico para manejar el estrés y la ansiedad.'
+    }
+  ];
+
+  /**
    * Formulario reactivo para la estrategia de apoyo al estudiante.
    * @protected
    * @type {FormGroup}
@@ -37,5 +71,16 @@ export class StudentSupportStrategyComponent {
     description: new FormControl('', [Validators.required])
   });
 
+  /**
+   * @param {StudentInfoService} studentInfoService servicio para obtener la información del estudiante
+   */
+  constructor(
+    private studentInfoService: StudentInfoService
+  ) {  }
+
+  ngOnInit() {
+    // Se obtiene la información del estudiante al inicializar el componente
+    this.studentInfo = this.studentInfoService.getStudentInfo();
+  }
 
 }
